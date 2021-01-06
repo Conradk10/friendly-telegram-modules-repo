92d2e4a2a004fb5553fc7789ed4a5ea097847ad6
Conradk10 <orion25rus@gmail.com>
2021-01-06 23:34:39 +0200

    Update hearts.py

--- hearts.py
+++ hearts.py
@@ -2,7 +2,7 @@ from .. import loader
 from asyncio import sleep
 @loader.tds
 class HeartsMod(loader.Module):
-	strings = {"name": "Heart's (fix)"}
+	strings = {"name": "Heart's"}
 	@loader.owner
 	async def heartscmd(self, message):
 		for _ in range(10):
